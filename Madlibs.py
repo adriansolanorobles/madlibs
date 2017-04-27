a5c47312377b2f1aea19deb4cc74bc2be22e0e13
adriansolanorobles <adrian.solano@protonmail.com>
2017-04-27 17:11:29 -0500

    Mad libs finished

---
+++ Madlibs.py
@@ -0,0 +1,39 @@
+
+"""
+historias con espacios en blanco asi el usuario 
+llenara con sus propias palabras
+"""
+print "The story has started"
+
+main_character = raw_input("Who is the main character ")
+
+adjective_one = raw_input("Firts adjective ")
+adjective_two = raw_input("Second adjective ")
+adjective_three = raw_input("Third adjective ")
+
+verb_one = raw_input("Verb one ")
+verb_two = raw_input("Verb two ")
+verb_three = raw_input("Verb three ")
+
+nouns_one = raw_input("Noun one ")
+nouns_two = raw_input("Noun two ")
+nouns_three = raw_input("Noun three ")
+nouns_four = raw_input("Noun four ")
+
+favorite_animal = raw_input("What is your favorite animal ")
+favorite_food = raw_input("What is your favorite food ")
+favorite_fruit = raw_input("What is your favorite fruit ")
+favorite_number = raw_input("What is your favorite number ")
+favorite_superhero = raw_input("What is your favorite superhero ")
+favorite_country = raw_input("What is your favorite country ")
+favorite_dessert = raw_input("What is your favorite dessert ")
+favorite_year = raw_input("What is your favorite year ")
+
+
+#The template for the story
+print "This morning I woke up and felt %s because %s was going to finally %s over the big %s. On the other side of the %s were many %s protesting to keep %s in stores. The crowd began to %s to the rythym of the %s, which made all of the %s very %s. %s tried to %s into the sewers and found %s rats. Needing help, %s quickly called %s. %s appeared and saved %s by flying to %s and dropping %s into a puddle of %s. %s then fell asleep and woke up in the year %s, in a world where %s ruled the world." % (main_character,adjective_one,adjective_two,adjective_three,verb_one,verb_two,verb_three,nouns_one,nouns_two,nouns_three,nouns_four,favorite_animal,favorite_food,favorite_fruit,favorite_number,favorite_superhero,favorite_country,favorite_country,favorite_dessert,favorite_year,nouns_one,nouns_one,nouns_one,favorite_dessert)
+
+
+
+
+
